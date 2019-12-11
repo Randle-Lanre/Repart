7d454e620b8b1dcab26beb49338a2a805797013a
Randle Kehinde Olanrewaju <kennyrandle09@gmail.com>
2019-12-11 13:51:22 +0000

    Add project files.

---
+++ Repart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Repart.MvcApplication" Language="C#" %>
---
+++ Repart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Repart.Startup))]
+namespace Repart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
