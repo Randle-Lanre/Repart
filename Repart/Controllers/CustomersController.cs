0abd4d30bc732d4d8bc40b0973a7ce4295a4e0b0
Randle Kehinde Olanrewaju <kennyrandle09@gmail.com>
2019-12-11 14:28:25 +0000

    added controllers for managing purchases, books and customers

---
+++ Repart/Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//this controller handles all the actions with regards to the customers
+namespace Repart.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Repart/Controllers/HomeController.cs
+++ Repart/Controllers/HomeController.cs
@@ -8,6 +8,20 @@ namespace Repart.Controllers
 {
     public class HomeController : Controller
     {
+        /*
+         * repart book store app would have the following
+         * there would be a list of books
+         * users can buy such books (select and click add to add to his or her account)
+         * on each purchase or collection by the user user the total number of books would be reduced(the
+         * particular variety of the book)
+         * the would be an admin(individuals designated as admin), would can add to the quantity of the books or remove
+         * and can also even remove the listing of the books
+         *
+         *model:    books, customers, genre (TODO: set relationships )
+         *
+         *navbar would be (customers, books )
+         *
+         */
         public ActionResult Index()
         {
             return View();
