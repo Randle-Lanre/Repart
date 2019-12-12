257c71940164ee316282f795df1f40eb68cbdede
Randle Kehinde Olanrewaju <kennyrandle09@gmail.com>
2019-12-12 11:31:00 +0000

    added view and application dbcontext for the movies controller

--- Repart/Controllers/BooksController.cs
+++ Repart/Controllers/BooksController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Repart.Models;
+
 //controller for handling book related activities
 namespace Repart.Controllers
 {
     public class BooksController : Controller
     {
+        //get the application dbcontext for the domain class
+        private ApplicationDbContext _context;
+
+        public BooksController()
+        {
+            _context= new ApplicationDbContext();
+        }
         // GET: Books
         public ActionResult Index()
         {
+            if
             return View();
         }
     }
